4dbda0c1ef8ce6229d956261d96d4e1ddb654ed7
Felipe Bilhares <felipe.bilhares@hotmail.com>
2018-03-15 16:08:35 -0400

    Alterações migrations

---
+++ eventos-api/src/main/resources/db/migration/V02__alter_tables.sql
@@ -0,0 +1,2 @@
+alter table tb_evento add foto blob;
+alter table tb_usuario add foto blob;
---
+++ eventos-api/src/main/resources/db/migration/V03__alter_tables2.sql
@@ -0,0 +1,2 @@
+alter table tb_evento MODIFY foto LONGBLOB;
+alter table tb_usuario MODIFY foto LONGBLOB;
