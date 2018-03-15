5fe589771d90e8fb8c8da445f15952314c4da886
Felipe Bilhares <felipe.bilhares@hotmail.com>
2018-03-15 09:26:59 -0400

    Autenticação por RFID

--- eventos-api/src/main/java/com/projeto/controller/RfidController.java
+++ eventos-api/src/main/java/com/projeto/controller/RfidController.java
@@ -2,12 +2,17 @@ package com.projeto.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
 import com.projeto.model.Rfid;
+import com.projeto.model.Usuario;
 import com.projeto.repository.RfidRepository;
 
 @Controller
@@ -15,6 +20,8 @@ public class RfidController {
 	
 	@Autowired
 	RfidRepository rfRepo;
+	@PersistenceContext
+	private EntityManager manager;
 	
 	
 	public boolean cadastrar(Rfid rfid) {
@@ -27,5 +34,21 @@ public class RfidController {
 	public Page<Rfid> buscarTodas(Pageable page) {
 		return rfRepo.findAll(page);
 		//return rfRepo.busca();
+	}
+	
+	public Usuario buscarUserByTag(Long id) {
+		Query q = null;
+
+		q = manager.createNativeQuery("select u.* from tb_evento e \r\n" + 
+				"join tb_ingresso_usuario i on (i.id_evento = e.id)\r\n" + 
+				"join tb_usuario u on (i.id_usuario = u.id)\r\n" + 
+				"where i.rfid_id = :id", Usuario.class);
+
+		q.setParameter("id", id);
+		
+		 Object singleResult = q.getSingleResult();
+
+		return (Usuario) singleResult;
+		
 	}
 }
--- eventos-api/src/main/java/com/projeto/resource/RfidResource.java
+++ eventos-api/src/main/java/com/projeto/resource/RfidResource.java
@@ -1,10 +1,12 @@
 package com.projeto.resource;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.controller.RfidController;
 import com.projeto.model.Rfid;
+import com.projeto.model.Usuario;
 
 @RestController
 @RequestMapping("/api")
@@ -28,7 +31,15 @@ public class RfidResource {
 
 	// busca todas as tags
 	@RequestMapping(value = "/rfid", method = RequestMethod.GET)
-	public Page<Rfid> cadastrarTag(Pageable page) {
+	public Page<Rfid> buscaTag(Pageable page) {
 		return rfControl.buscarTodas(page);
+	}
+
+	// busca todas as tags
+	@RequestMapping(value = "/rfid/{id}", method = RequestMethod.GET)
+	public Usuario buscaUser(@PathVariable("id") String id) {
+		BigInteger bi = new BigInteger(id, 16);
+		System.out.println(bi);
+		return rfControl.buscarUserByTag(Long.parseLong(bi.toString()));
 	}
 }
